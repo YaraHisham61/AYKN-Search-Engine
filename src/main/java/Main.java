e154e10597a9c53351f75ca5cb8b70e2990a165d
AhmedSamy02 <https://github.com/AhmedSamy02>
2023-04-25 04:16:16 +0200

    Indexer level 2

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,7 +3,6 @@ import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
-import org.bson.Document;
 
 import java.io.IOException;
 import java.util.concurrent.BrokenBarrierException;
